2265b45ada8b749463daf11dbfc655d154e761e5
Abramov Artem <62434325+ChillyNerd@users.noreply.github.com>
2025-08-27 17:27:33 +0500

    added requirement tree

---
+++ requirements_tree/main.py
@@ -0,0 +1,15 @@
+from requirements_tree.utils.requirement_tree import RequirementTree
+from requirements_tree.utils.service import Service
+
+services_json = [{'name': 'discovery', 'required': ['config']}, {'name': 'config', 'required': []}, {'name': 'auth', 'required': ['discovery']}, {'name': 'api-gateway', 'required': ['discovery']}, {"name": "menu", "required": ["api-gateway", "auth"]}]
+services = list(map(lambda parameters: Service(parameters), services_json))
+tree = RequirementTree()
+tree.parse_requirements(services)
+priority_dict = tree.get_priority_dict()
+services_with_priority = list(map(
+    lambda service: Service({'priority': priority_dict[service.name]}).copy_from(service), services
+))
+sorted_services = sorted(services_with_priority, key=lambda service: service.priority)
+print(list(map(
+    lambda service: (service.name, service.priority), sorted_services
+)))
